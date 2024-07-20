1fd86c6f42f4a9ec71c91a9b28e064b669c0cad9
Robert Luiten <156024622+RobertLuiten@users.noreply.github.com>
2024-07-19 17:59:22 -0700

    Fixed tab issue

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,11 +1,9 @@
 "use client"
 
 import { Candidate } from "@/electionComponents/candidate";
-import { Contest, ContestProps } from "@/electionComponents/contest";
-import { Election, ElectionProps } from "@/electionComponents/election";
+import { ContestProps } from "@/electionComponents/contest";
+import { ElectionProps } from "@/electionComponents/election";
 import { EligibleElections, EligibleElectionsProps } from "@/electionComponents/eligibleElections";
-import React, { Component, useState } from "react";
-import ReactDOM from "react-dom";
 
 /**
  * This is a simple proof of concept I set up, feel free to mess with it as needed!
@@ -14,16 +12,16 @@ import ReactDOM from "react-dom";
 export default function Home() {
 
   //Hard coded candidates for the demo
-  const can1 = new Candidate({candidate_name : "Blinky"});
-  const can2 = new Candidate({candidate_name : "Evil Robot Bear"});
+  const can1 = new Candidate({candidate_name : "Rob"});
+  const can2 = new Candidate({candidate_name : "David"});
   const can3 = new Candidate({candidate_name : "Owen Varang"});
-  const can4 = new Candidate({candidate_name : "Snorky"});
+  const can4 = new Candidate({candidate_name : "Anaya"});
   const can5 = new Candidate({candidate_name : "Blinky"});
-  const can6 = new Candidate({candidate_name : "Good Robot Bear"});
-  const can7 = new Candidate({candidate_name : "Aaron Harris"});
-  const can8 = new Candidate({candidate_name : "Robert Luiten"});
-  const can9 = new Candidate({candidate_name : "Jack"});
-  const can10 = new Candidate({candidate_name : "Saul Coolman"});
+  const can6 = new Candidate({candidate_name : "The Robot Bear"});
+  const can7 = new Candidate({candidate_name : "Jeff Bezos"});
+  const can8 = new Candidate({candidate_name : "Ana Mari Cauce"});
+  const can9 = new Candidate({candidate_name : "Penix"});
+  const can10 = new Candidate({candidate_name : "VeeBee"});
   const can11 = new Candidate({candidate_name : "Zeus"});
   const can12 = new Candidate({candidate_name : "Joey"});
 
@@ -31,9 +29,9 @@ export default function Home() {
   const candidates1 = [can1, can2, can3, can4];
   const candidates2 = [can5, can6, can7, can8];
   const candidates3 = [can9, can10, can11, can12];
-  const contestinfo1 : ContestProps = {contest_name : "Robland Mayor", candidates : candidates1};
-  const contestinfo2 : ContestProps = {contest_name : "Robland Sewer Cleaner", candidates : candidates2};
-  const contestinfo3 : ContestProps = {contest_name : "Brownieland Police Guy", candidates : candidates3};
+  const contestinfo1 : ContestProps = {contest_name : "Seattle Mayor", candidates : candidates1};
+  const contestinfo2 : ContestProps = {contest_name : "Robville Mayor", candidates : candidates2};
+  const contestinfo3 : ContestProps = {contest_name : "The Sherrif", candidates : candidates3};
 
   //Election props are basically collections of Contests
   const electioninfo1 : ElectionProps = {election_type : "General Election", contests : [contestinfo1, contestinfo2, contestinfo3]};
